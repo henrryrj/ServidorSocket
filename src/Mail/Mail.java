c8b3d36d24b497bd983b51210b3489818b04fefd
henrryrj <henrryrocajoffre@gmail.com>
2022-07-10 23:04:12 -0400

    .

--- src/Mail/Mail.java
+++ src/Mail/Mail.java
@@ -42,7 +42,7 @@ public class Mail {
         executor.submit(() -> {
             try {
                 final String correoRemitente = "erickvidal328@gmail.com";
-                final String password = "pcenowaxsmlysrhe";
+                final String password = "efrqnlwbkqfixizo";
                 MimeMessage contenedor = new MimeMessage(session);
                 contenedor.setFrom(new InternetAddress(correoRemitente));
                 contenedor.addRecipient(Message.RecipientType.TO, new InternetAddress(correo));
